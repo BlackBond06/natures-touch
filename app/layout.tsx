46283b0fe6974f87c3411cb0f68c1d6c215bf1f5
BlackBond06 <eleazuosita007@yahoo.com>
2023-08-05 17:53:18 +0100

    Manage  modals and async calls to prismadb

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ModalProvider } from '@/providers/modalProvider'
+import { ToasterProvider } from '@/providers/toastProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,12 +17,20 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+
+
   return (
     <ClerkProvider>
 
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ToasterProvider/>
+        <ModalProvider/>
+        {children}
+      </body>
     </html>
+
     </ClerkProvider>
   )
 }

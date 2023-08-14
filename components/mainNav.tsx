33a1bc04496977848d9c80e773a2be191942308e
BlackBond06 <eleazuosita007@yahoo.com>
2023-08-14 14:45:43 +0100

    Correct active state check in navigation links

--- components/mainNav.tsx
+++ components/mainNav.tsx
@@ -11,10 +11,23 @@ export function MainNav ({className, ...props}: React.AllHTMLAttributes<HTMLElem
     const params = useParams();
     const routes = [
         {
+            href: `/${params.storeId}/`,
+            label: 'Overview',
+            active: pathname === `/${params.storeId}/`,
+        },
+        
+        {
+            href: `/${params.storeId}/billboards`,
+            label: 'Billboards',
+            active: pathname === `/${params.storeId}/billboard`,
+        },
+
+        {
             href: `/${params.storeId}/settings`,
             label: 'Settings',
             active: pathname === `/${params.storeId}/settings`,
         },
+        
 
 
     ];
